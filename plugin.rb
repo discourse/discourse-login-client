b0a585cc865cbc746444a9db4d58250878460216
zogstrip <regis@hanol.fr>
2025-05-20 20:04:49 +0200

    FIX: automatically skip create confirm when plugin is enabled
    This is dependent on https://github.com/discourse/discourse/pull/32824 being merged.

--- plugin.rb
+++ plugin.rb
@@ -14,3 +14,15 @@ require_relative "lib/discourse_login_client_authenticator"
 enabled_site_setting :discourse_login_client_enabled
 
 auth_provider icon: "fab-discourse", authenticator: DiscourseLoginClientAuthenticator.new
+
+after_initialize do
+  on_enabled_change do |_, enabled|
+    if enabled
+      SiteSetting.set(:auth_skip_create_confirm, true)
+      SiteSetting.hidden_settings_provider.add_hidden(:auth_skip_create_confirm)
+    else
+      SiteSetting.set(:auth_skip_create_confirm, false)
+      SiteSetting.hidden_settings_provider.remove_hidden(:auth_skip_create_confirm)
+    end
+  end
+end
