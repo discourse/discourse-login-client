2e74ff77be1bb2e78f1f500937204060fcdd4730
Penar Musaraj <pmusaraj@gmail.com>
2025-06-05 15:50:09 -0400

    update spec

--- spec/system/discourse_login_client_spec.rb
+++ spec/system/discourse_login_client_spec.rb
@@ -27,10 +27,24 @@ describe "discourse login client auth" do
   let(:signup_form) { PageObjects::Pages::Signup.new }
 
   context "when user does not exist" do
-    it "skips the signup form and creates the account directly" do
-      visit("/")
-      signup_form.open.click_social_button("discourse_login")
-      expect(page).to have_css(".header-dropdown-toggle.current-user")
+    context "when auth_skip_create_confirm is false" do
+      before { SiteSetting.auth_skip_create_confirm = false }
+
+      it "skips the signup form and creates the account directly" do
+        visit("/")
+        signup_form.open.click_social_button("discourse_login")
+        expect(page).to have_css(".login-welcome-header")
+      end
+    end
+
+    context "when auth_skip_create_confirm is true" do
+      before { SiteSetting.auth_skip_create_confirm = true }
+
+      it "skips the signup form and creates the account directly" do
+        visit("/")
+        signup_form.open.click_social_button("discourse_login")
+        expect(page).to have_css(".header-dropdown-toggle.current-user")
+      end
     end
   end
 
